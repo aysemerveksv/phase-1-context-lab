c64aedf6f007e287885a2cf0f6c9c478f6ead5d4
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-05-01 22:23:59 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
-
+function createEmployeeRecord(recordArray) {
+    return {
+      firstName: recordArray[0],
+      familyName: recordArray[1],
+      title: recordArray[2],
+      payPerHour: recordArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  function createEmployeeRecords(recordsArray) {
+    return recordsArray.map(record => createEmployeeRecord(record))
+  }
+  
+  function createTimeInEvent(employeeRecord, timeStamp) {
+    let [date, hour] = timeStamp.split(" ")
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+    })
+    return employeeRecord
+  }
+  
+  function createTimeOutEvent(employeeRecord, timeStamp) {
+    let [date, hour] = timeStamp.split(" ")
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    })
+    return employeeRecord
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    let timeIn = employeeRecord.timeInEvents.find(e => e.date === date).hour
+    let timeOut = employeeRecord.timeOutEvents.find(e => e.date === date).hour
+    return (timeOut - timeIn) / 100
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    let hoursWorked = hoursWorkedOnDate(employeeRecord, date)
+    return hoursWorked * employeeRecord.payPerHour
+  }
+  
+  function allWagesFor(employeeRecord) {
+    let availableDates = employeeRecord.timeInEvents.map(e => e.date)
+    let totalPay = availableDates.reduce((totalPay, date) => totalPay + wagesEarnedOnDate(employeeRecord, date), 0)
+    employeeRecord.allWages = totalPay // add a new property to the employeeRecord with the total wages
+    return totalPay
+  }
+  
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0)
+  }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
